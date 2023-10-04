24593124780741fafd54855490842ca823c8846d
Sadiq <sadiq.said@student.moringaschool.com>
2023-10-04 12:23:32 +0300

    Assignment completed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print('[]')
+    elif length == 1:
+        print('[0]')
+    else:
+        # Initialize the sequence with the first two elements
+        sequence = [0, 1]
+
+        # Calculate the rest of the Fibonacci sequence
+        for i in range(2, length):
+            next_num = sequence[-1] + sequence[-2]
+            sequence.append(next_num)
+
+        # Print the Fibonacci sequence
+        print(sequence)
+
+
+# Run the function with the provided test cases
+print_fibonacci(0)
+print_fibonacci(1)
+print_fibonacci(2)
+print_fibonacci(10)
